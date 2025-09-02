6146697707670088d0c7249f5614ebc42443d633
Soham Jobanputra <soham.jobanputra@prominentpixel.com>
2025-09-02 10:26:58 +0530

    change: changed to relative imports instead of alias imports

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,13 +1,13 @@
 import { Moon, Sun } from "lucide-react"
 
-import { Button } from "@/components/ui/button"
+import { Button } from "./ui/button.tsx"
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { useTheme } from "@/components/theme-provider"
+} from "./ui/dropdown-menu.tsx"
+import { useTheme } from "./theme-provider.tsx"
 
 export function ModeToggle() {
   const { setTheme } = useTheme()
